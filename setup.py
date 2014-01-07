751eedf1e109d83baefff0150d1144c65c96e24c
Christopher Rosell <chrippa@tanuki.se>
2014-01-07 21:58:53 +0100

    Add url to setup.py.

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python
 
 from setuptools import setup
 
 setup(name="ds4drv",
       version="0.1.0",
       description="A DualShock 4 bluetooth driver for Linux",
+      url="https://github.com/chrippa/ds4drv",
       author="Christopher Rosell",
       author_email="chrippa@tanuki.se",
       license="MIT",
