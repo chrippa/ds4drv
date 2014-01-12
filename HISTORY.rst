ede3a4026cb2d6f16651b5582c60a8e0054dc3e8
Christopher Rosell <chrippa@tanuki.se>
2014-01-12 14:39:21 +0100

    Release version 0.1.1.

---
+++ HISTORY.rst
@@ -0,0 +1,17 @@
+
+Release history
+---------------
+
+0.1.1
+^^^^^
+
+- Fixed incorrect dpad parsing.
+- Handle uinput errors instead of printing exception.
+
+
+0.1.0
+^^^^^
+
+- First release.
+
+
--- setup.py
+++ setup.py
@@ -2,17 +2,30 @@
 
 from setuptools import setup
 
+readme = open("README.rst").read()
+history = open("HISTORY.rst").read()
+
 setup(name="ds4drv",
-      version="0.1.0",
+      version="0.1.1",
       description="A DualShock 4 bluetooth driver for Linux",
       url="https://github.com/chrippa/ds4drv",
       author="Christopher Rosell",
       author_email="chrippa@tanuki.se",
       license="MIT",
+      long_description=readme + "\n\n" + history,
       entry_points={
-          "console_scripts": ["ds4drv=ds4drv:main"]
+        "console_scripts": ["ds4drv=ds4drv:main"]
       },
       py_modules=["ds4drv"],
       install_requires=["evdev"],
+      classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.3",
+        "Topic :: Games/Entertainment"
+      ]
 )
 
