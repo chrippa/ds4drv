ae5a6c38ecf99403fb3d3a08416f7296b0ded6c1
Christopher Rosell <chrippa@tanuki.se>
2014-01-29 00:43:24 +0100

    Fix syntax error.

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(name="ds4drv",
       entry_points={
         "console_scripts": ["ds4drv=ds4drv.__main__:main"]
       },
-      packages=["ds4drv", "ds4drv.backends"]
+      packages=["ds4drv", "ds4drv.backends"],
       install_requires=["evdev", "pyudev"],
       classifiers=[
         "Development Status :: 4 - Beta",
