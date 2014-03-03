37e7791368a0b27f1ffcc9119f3d5dcf70c55e29
Christopher Rosell <chrippa@tanuki.se>
2014-03-03 22:48:01 +0100

    eventloop: Fire events immediately.

--- ds4drv/eventloop.py
+++ ds4drv/eventloop.py
@@ -1,34 +1,11 @@
 import os
-import ctypes
 
 from collections import defaultdict, deque
 from functools import wraps
 from select import epoll, EPOLLIN
-from struct import Struct
 
 from .packages import timerfd
 from .utils import iter_except
-
-
-class eventfd(object):
-    """Simple wrapper around the eventfd syscall."""
-
-    libc = ctypes.CDLL(ctypes.util.find_library("c"))
-    struct = Struct("Q")
-
-    def __init__(self, value=0, flags=0):
-        self.fd = self.libc.eventfd(value, flags)
-
-    def fileno(self):
-        return self.fd
-
-    def write(self, value):
-        buf = self.struct.pack(value)
-        os.write(self.fd, buf)
-
-    def read(self):
-        buf = os.read(self.fd, 8)
-        return self.struct.unpack(buf)[0]
 
 
 class EventLoop(object):
@@ -80,21 +57,17 @@ class EventLoop(object):
     def unregister_event(self, event, callback):
         self.event_callbacks[event].remove(callback)
 
-    def fire_event(self, event, *args):
+    def fire_event(self, event, *args, **kwargs):
         self.event_queue.append((event, args))
-        self.event_fd.write(1)
+        self.process_events()
 
     def process_events(self):
         for event, args in iter_except(self.event_queue.popleft, IndexError):
             for callback in self.event_callbacks[event]:
                 callback(*args)
 
-        self.event_fd.read()
-
     def run(self):
-        self.add_watcher(self.event_fd, self.process_events)
         self.running = True
-
         while self.running:
             for fd, event in self.epoll.poll():
                 callback = self.callbacks.get(fd)
@@ -106,7 +79,6 @@ class EventLoop(object):
         self.callbacks = {}
         self.epoll = epoll()
 
-        self.event_fd = eventfd()
         self.event_queue = deque()
         self.event_callbacks = defaultdict(set)
 
