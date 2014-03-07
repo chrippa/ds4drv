f113e460894a2e931695c200b9994a1e616115f3
Christopher Rosell <chrippa@tanuki.se>
2014-03-07 01:58:56 +0100

    Release version 0.5.0.

--- ds4drv/__init__.py
+++ ds4drv/__init__.py
@@ -1,7 +1,7 @@
 """ds4drv is a Sony DualShock 4 userspace driver for Linux."""
 
 __title__ = "ds4drv"
-__version__ = "0.4.3"
+__version__ = "0.5.0"
 __author__ = "Christopher Rosell"
 __credits__ = ["Christopher Rosell", "George Gibbs", "Lauri Niskanen"]
 __license__ = "MIT"
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ readme = open("README.rst").read()
 history = open("HISTORY.rst").read()
 
 setup(name="ds4drv",
-      version="0.4.3",
+      version="0.5.0",
       description="A Sony DualShock 4 userspace driver for Linux",
       url="https://github.com/chrippa/ds4drv",
       author="Christopher Rosell",
